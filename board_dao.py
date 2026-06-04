bd8390cbe1a9ec647a675b7ff156a58dce34531c
admin <admin@admin.comm>
2026-06-04 17:35:30 +0900

    first commit

--- board_dao.py
+++ board_dao.py
@@ -14,6 +14,23 @@ class BoardDAO:
             database=self.database,
             charset="utf8mb4" # main.py와 연결
         )
+    def insert_board(self,title,content,writer):
+        conn=self.get_connection()
+        cursor=conn.cursor()
+        sql="""
+        INSERT INTO board
+        (title,content,writer)
+        VALUES
+        (%s,%s,%s)
+        """
+        cursor.execute(
+            sql,
+            (title,content,writer)
+        )
+        conn.commit()
+        cursor.close()
+        conn.close()
+
     def select_all(self):
         conn =self.get_connection()
         cursor =conn.cursor()
@@ -29,4 +46,30 @@ class BoardDAO:
         conn.close()
 
         return result
+    def select_one(self,board_id):
+        conn = self.get_connection()
+        cursor=conn.cursor()
+        sql="""
+        SELECT *
+        FROM board
+        WHERE id=%s
+        """
+        cursor.execute(sql,(board_id,))
+        result=cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return result
+    def delete_board(self, board_id):
+        conn = self.get_connection()
+        cursor= conn.cursor()
+        sql="""
+        DELETE
+        FROM board
+        WHERE id=%s
+        """
+        cursor.execute(sql,(board_id))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("삭제 완료")
         
--- main.py
+++ main.py
@@ -20,5 +20,29 @@ while True:
                   board[1],
                   board[2],
                   board[3])
+    elif menu == "2":
+        title =input("제목 : ")
+        content = input("내용 : ")
+        writer = input("작성자 : ")
+        board_dao.insert_board(
+            title,
+            content,
+            writer
+        )
+        print("등록 완료")
+    elif menu == "3":
+        num = input("번호 : ")
+        board = board_dao.select_one(num)
+        if board:
+            print()
+            print("번호 : ",board[0])
+            print("제목 : ",board[1])
+            print("내용 : ",board[2])
+            print("작성자 : ",board[3])
+            print("작성일 : ",board[4])
+    elif menu == "4":
+        num = input("삭제 번호 : ")
+        board_dao.delete_board(num)
+        print("삭제 완료")
     
 print("게시판 종료")
